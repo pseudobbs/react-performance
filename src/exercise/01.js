2c58d6c10f980cf89fef381d596eabf77a06a729
Shawn Dobbs <pseudobbs@protonmail.com>
2023-08-25 12:03:59 -0500

    Exercise 1 with EC

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,20 +1,15 @@
 // Code splitting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
-// 💣 remove this import
-import Globe from '../globe'
+import * as React from 'react';
+import {useState} from 'react';
+import {lazy} from 'react';
 
-// 🐨 use React.lazy to create a Globe component which uses a dynamic import
-// to get the Globe component from the '../globe' module.
+const Globe = lazy(() => import(/*webpackPrefetch: true*/ '../globe'));
 
 function App() {
-  const [showGlobe, setShowGlobe] = React.useState(false)
+  const [showGlobe, setShowGlobe] = useState(false);
 
-  // 🐨 wrap the code below in a <React.Suspense /> component
-  // with a fallback.
-  // 💰 try putting it in a few different places and observe how that
-  // impacts the user experience.
   return (
     <div
       style={{
@@ -23,7 +18,7 @@ function App() {
         flexDirection: 'column',
         justifyContent: 'center',
         height: '100%',
-        padding: '2rem',
+        padding: '2rem'
       }}
     >
       <label style={{marginBottom: '1rem'}}>
@@ -34,14 +29,13 @@ function App() {
         />
         {' show globe'}
       </label>
-      <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
-      </div>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <div style={{width: 400, height: 400}}>
+          {showGlobe ? <Globe /> : null}
+        </div>
+      </React.Suspense>
     </div>
-  )
+  );
 }
-// 🦉 Note that if you're not on the isolated page, then you'll notice that this
-// app actually already has a React.Suspense component higher up in the tree
-// where this component is rendered, so you *could* just rely on that one.
 
-export default App
+export default App;
