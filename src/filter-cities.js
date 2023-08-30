b88265e9314091b1e34e558e33e4c85b372d9863
Shawn Dobbs <pseudobbs@protonmail.com>
2023-08-30 09:29:38 -0500

    Exercise 4

--- src/filter-cities.js
+++ src/filter-cities.js
@@ -8,7 +8,6 @@ const allItems = cities.map((city, index) => ({
 
 // for some reason workerize doesn't like export {getItems}
 // but it's fine with inline exports like this so that's what we're doing.
-// comment
 export function getItems(filter) {
   if (!filter) {
     return allItems;
